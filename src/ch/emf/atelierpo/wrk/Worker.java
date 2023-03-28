84c7944c2667c2a50f7986b3351b6a2101227c40
Bajramil <Bajramil@WSTEMFB32-15.stu.net.fr.ch>
2023-03-28 09:27:35 +0200

    Javadoc

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,7 +6,18 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
+   /**
+    * La méthode permet de contrôler les plaques d'immatriculation avec
+    * des contraintes spécifique : /n
+    * Le canton n'accepte que les caractères A-Z en majuscule et 
+    * accepte que les cantons existants qui sont écrit avec deux
+    * caractères uniquement /n
+    * Le numero de la plaque doit être un nombre entier
+    * @param canton Chaine de caractère caractèrisant le canton 
+    * @param numeroDePlaque chaîne de caractère caractèrisant le numéro de plaque 
+    * @return vrai si le canton et la plaque correspondent aux critères donnés
+    * sinon on retourne faux.
+    */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
         return false;
     }
